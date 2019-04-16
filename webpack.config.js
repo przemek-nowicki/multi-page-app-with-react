418ef3354efdca2ce91305400cb8cf466f099a91
Przemek Nowicki <nowicki.przemek@gmail.com>
2019-04-16 16:43:48 +0200

    split vendor libs into a separate file

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,19 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const getFilesFromDir = require('./config/files');
-
+const getFilesFromDir = require("./config/files");
 const PAGE_DIR = path.join("src", "pages", path.sep);
 
 const htmlPlugins = getFilesFromDir(PAGE_DIR, [".html"]).map( filePath => {
   const fileName = filePath.replace(PAGE_DIR, "");
-  // { chunks:['contact'], template: 'src/pages/contact.html',  filename: 'contact.html'}
+  // { chunks:["contact", "vendor"], template: "src/pages/contact.html",  filename: "contact.html"}
   return new HtmlWebPackPlugin({
-    chunks:[fileName.replace(path.extname(fileName), "")],
+    chunks:[fileName.replace(path.extname(fileName), ""), "vendor"],
     template: filePath,
     filename: fileName
   })
 });
 
-// { contact: './src/pages/contact.js' }
+// { contact: "./src/pages/contact.js" }
 const entry = getFilesFromDir(PAGE_DIR, [".js"]).reduce( (obj, filePath) => {
   const entryChunkName = filePath.replace(path.extname(filePath), "").replace(PAGE_DIR, "");
   obj[entryChunkName] = `./${filePath}`;
@@ -45,5 +44,17 @@ module.exports = {
 					}
 				},
       }]
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /node_modules/,
+            chunks: "initial",
+            name: "vendor",
+            enforce: true
+          }
+        }
+      }
     }
 };
