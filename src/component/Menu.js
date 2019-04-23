611b35bfebf31c99283dfd03a00a9262b5793eaa
Przemek Nowicki <nowicki.przemek@gmail.com>
2019-04-23 21:40:51 +0200

    Added menu items

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 export default class Menu extends Component {
     render() {
-        return (<div>Menu is working</div>);
+        return (
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="products/product-1.html">Product</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        );
     }
-}    
+}
